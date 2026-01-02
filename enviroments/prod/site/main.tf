7ed703a7b1700977749c816c05c06d0c66673eed
kallum-cooper <kallumcooper97@gmail.com>
2026-01-02 23:16:57 +0000

    S3 module intergration

--- enviroments/prod/site/main.tf
+++ enviroments/prod/site/main.tf
@@ -7,3 +7,13 @@
 # - DNS records to CloudFront
 
 # Placeholder output is in outputs.tf
+
+module "origin_bucket" {
+  source = "git::https://github.com/kallum-cooper/terraform-modules.git//modules/s3-private-origin-bucket?ref=v0.1.0"
+
+  bucket_name        = "kc-${var.environment}-site-origin-${data.aws_caller_identity.current.account_id}"
+  versioning_enabled = false
+  tags               = local.tags
+}
+
+data "aws_caller_identity" "current" {}
