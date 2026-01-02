8743d108e861b0bdbbab296366229a725cf8a781
kallum-cooper <kallumcooper97@gmail.com>
2026-01-02 21:20:40 +0000

    ACM cert + DNS validation

---
+++ enviroments/prod/site/acm.tf
@@ -0,0 +1,37 @@
+resource "aws_acm_certificate" "site" {
+  provider = aws.use1
+
+  domain_name               = local.root_domain
+  subject_alternative_names = ["www.${local.root_domain}"]
+  validation_method         = "DNS"
+
+  tags = local.tags
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_route53_record" "acm_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.site.domain_validation_options :
+    dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  zone_id = aws_route53_zone.root.zone_id
+  name    = each.value.name
+  type    = each.value.type
+  ttl     = 60
+  records = [each.value.record]
+}
+
+resource "aws_acm_certificate_validation" "site" {
+  provider = aws.use1
+
+  certificate_arn         = aws_acm_certificate.site.arn
+  validation_record_fqdns = [for r in aws_route53_record.acm_validation : r.fqdn]
+}
--- enviroments/prod/site/outputs.tf
+++ enviroments/prod/site/outputs.tf
@@ -7,3 +7,8 @@ output "route53_name_servers" {
   description = "Set these as the domain's nameservers at registrar for domain."
   value       = aws_route53_zone.root.name_servers
 }
+
+output "acm_certificate_arn" {
+  description = "ACM certificate ARN in us-east-1 (for CloudFront)."
+  value       = aws_acm_certificate.site.arn
+}
