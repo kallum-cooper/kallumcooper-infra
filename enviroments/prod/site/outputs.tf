f5a639af65a78b7def8991aeabb59aed4bae5f7a
kallum-cooper <kallumcooper97@gmail.com>
2026-01-03 20:29:42 +0000

    Module output change

--- enviroments/prod/site/outputs.tf
+++ enviroments/prod/site/outputs.tf
@@ -16,3 +16,28 @@ output "acm_certificate_arn" {
 output "route53_zone_id" {
   value = aws_route53_zone.root.zone_id
 }
+
+output "origin_bucket_name" {
+  value       = module.origin_bucket.bucket_name
+  description = "S3 origin bucket name."
+}
+
+output "origin_bucket_arn" {
+  value       = module.origin_bucket.bucket_arn
+  description = "S3 origin bucket ARN."
+}
+
+output "origin_bucket_regional_domain" {
+  value       = module.origin_bucket.bucket_regional_domain_name
+  description = "S3 regional domain name used as the CloudFront origin."
+}
+
+output "cloudfront_domain_name" {
+  value       = module.cdn.distribution_domain_name
+  description = "CloudFront distribution domain name."
+}
+
+output "cloudfront_distribution_id" {
+  value       = module.cdn.distribution_id
+  description = "CloudFront distribution ID."
+}
