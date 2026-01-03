f87442a7619c49af5fbea91d255f63f475e9a673
kallum-cooper <kallumcooper97@gmail.com>
2026-01-03 20:29:14 +0000

    Module output change

--- enviroments/prod/site/main.tf
+++ enviroments/prod/site/main.tf
@@ -8,7 +8,7 @@
 
 
 module "origin_bucket" {
-  source = "git::https://github.com/kallum-cooper/terraform-modules.git//modules/s3-private-origin-bucket?ref=v0.1.0"
+  source = "git::https://github.com/kallum-cooper/terraform-modules.git//modules/s3-private-origin-bucket?ref=v0.1.1"
 
   bucket_name        = "kc-${var.environment}-site-origin-${data.aws_caller_identity.current.account_id}"
   versioning_enabled = false
